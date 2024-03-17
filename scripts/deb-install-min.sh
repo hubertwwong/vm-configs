cd5d297b5a0db2a47b7ab221915f15c2bb2d6827
Hubert Wong <foo@bar.com>
2024-03-17 12:20:59 -0700

    Cleanup

--- scripts/deb-install-min.sh
+++ scripts/deb-install-min.sh
@@ -11,20 +11,11 @@ function is_defined {
     fi
 }
 
+echo "> Validate variables"
 is_defined 'SUDO_PASS' $SUDO_PASS
 is_defined 'GIT_EMAIL' $GIT_EMAIL
 is_defined 'GIT_PASSWORD' $GIT_NAME
 is_defined 'GIT_BRANCH' $GIT_BRANCH
-
-#export SUDO_PASS=changeme 
-#export GIT_EMAIL="foo@bar.com"
-#export GIT_NAME="Hubert Wong"
-INSTALL_DIR=/tmp/vm-configs
-
-# echo "> Recreating the install directory"
-# rm -rf /tmp/vm-configs
-# mkdir -p /tmp/vm-configs
-# cd /tmp/vm-configs
 
 echo "> Install ansible and git"
 echo $SUDO_PASS | sudo -S apt update && echo $SUDO_PASS | sudo -S apt -y upgrade 
